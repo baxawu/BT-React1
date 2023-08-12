b5b1d221e4570ec288cc70a006c83834e0713983
PhiLe <jkj822925@gmail.com>
2023-08-12 23:33:17 +0800

    first commit

---
+++ src/Ex_react/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+  <nav className="navbar navbar-expand-lg bg-dark">
+    <div className="container-fluid">
+      <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse justify-content-end"  id="navbarNav" >
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link text-white active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
+
+  )
+}
